19710e085848be333a20390b4635e0cc501cad26
avatarannappa <nkcs.chenhao@gmail.com>
2020-01-22 14:48:32 +0800

    '+add,16,最接近的三数之和'

--- src/main/java/ThreeSumClosest16.java
+++ src/main/java/ThreeSumClosest16.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 16. 最接近的三数之和.
  *
@@ -7,12 +9,31 @@
 public class ThreeSumClosest16 {
 
     public int threeSumClosest(int[] nums, int target) {
-        return 0;
+        Arrays.sort(nums);
+        int sum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int temp = nums[i] + nums[start] + nums[end];
+                if (Math.abs(target - temp) < Math.abs(target - sum)) {
+                    sum = temp;
+                }
+                if (temp > target) {
+                    end--;
+                } else if (temp < target) {
+                    start++;
+                } else {
+                    return temp;
+                }
+            }
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
-        int[] nums = {-1, 0, 1, -4};
-        int target = 1;
+        int[] nums = {1, 2, 4, 8, 16, 32, 64, 128};
+        int target = 82;
         System.out.println(new ThreeSumClosest16().threeSumClosest(nums, target));
     }
 }
