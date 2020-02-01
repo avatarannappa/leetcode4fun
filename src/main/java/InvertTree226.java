b6ba3cf8844a87355c2a0e6932eada66cd4aec78
avatarannappa <nkcs.chenhao@gmail.com>
2020-02-01 19:16:39 +0800

    '+add,226,翻转二叉树'

---
+++ src/main/java/InvertTree226.java
@@ -0,0 +1,22 @@
+/**
+ * 226. 翻转二叉树
+ * <p>
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ *
+ * @author avatarannappa
+ * @version 1.0, 2020/2/1
+ */
+public class InvertTree226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
