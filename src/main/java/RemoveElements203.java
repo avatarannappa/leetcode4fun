1e7126197c758b34bd494a89cb85d2081913e451
avatarannappa <nkcs.chenhao@gmail.com>
2020-02-18 14:25:05 +0800

    #update,203,移除链表元素,origin没有用哨兵

--- src/main/java/RemoveElements203.java
+++ src/main/java/RemoveElements203.java
@@ -6,8 +6,31 @@
  */
 public class RemoveElements203 {
     public ListNode removeElements(ListNode head, int val) {
+        ListNode start = head;
+        ListNode pre = null;
+        while (head != null) {
+            if (pre == null) {
+                if (head.val == val) {
+                    start = head.next;
+                } else {
+                    pre = head;
+                }
+            } else {
+                if (head.val == val) {
+                    pre.next = head.next;
+                } else {
+                    pre = head;
+                }
+            }
+            head = head.next;
+        }
+        return start;
+    }
+
+    public ListNode remove(ListNode head, int val) {
         // 当要删除的一个或多个节点位于链表的头部时，事情会变得复杂。
-        // 可以通过哨兵节点去解决它，哨兵节点广泛应用于树和链表中，如伪头、伪尾、标记等，它们是纯功能的，通常不保存任何数据，其主要目的是使链表标准化，如使链表永不为空、永不无头、简化插入和删除
+        // 可以通过哨兵节点去解决它，哨兵节点广泛应用于树和链表中.
+        // 如伪头、伪尾、标记等，它们是纯功能的，通常不保存任何数据，其主要目的是使链表标准化，如使链表永不为空、永不无头、简化插入和删除
         ListNode sentinel = new ListNode(0);
         sentinel.next = head;
 
