255228f79ff5ad12171f51b496da3a06023d7f73
avatarannappa <nkcs.chenhao@gmail.com>
2020-03-15 00:09:59 +0800

    +add,292,Nim 游戏

--- src/main/java/CanWinNim292.java
+++ src/main/java/CanWinNim292.java
@@ -1,6 +1,16 @@
 /**
- * @author chenhao15
- * @version 1.0, 2020/3/13
+ * 292. Nim 游戏.
+ *
+ * @author avatarannappa
+ * @version 1.0, 2020/3/15
  */
 public class CanWinNim292 {
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new CanWinNim292().canWinNim(4));
+    }
 }
