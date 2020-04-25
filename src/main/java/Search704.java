b00bf9b750e212c5004e86f27ad47dc8feb0c1de
avatarannappa <nkcs.chenhao@gmail.com>
2020-04-25 18:49:42 +0800

    +add,704,二分查找

---
+++ src/main/java/Search704.java
@@ -0,0 +1,33 @@
+/**
+ * 704. 二分查找
+ *
+ * @author avatarannappa
+ * @version 1.0, 2020/4/25
+ */
+public class Search704 {
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] > target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        System.out.println(new Search704().search(nums, target));
+    }
+}
