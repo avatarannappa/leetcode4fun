51f1ee85154b91e3628f038c39fa49d8bb422c5f
avatarannappa <nkcs.chenhao@gmail.com>
2020-04-28 11:55:47 +0800

    +add,51,N皇后,回溯

--- src/main/java/SolveNQueens51.java
+++ src/main/java/SolveNQueens51.java
@@ -1,8 +1,97 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * @author chenhao15
- * @version 1.0, 2020/4/26
+ * 51. N皇后.
+ *
+ * @author avatarannappa
+ * @version 1.0, 2020/4/28
  */
 public class SolveNQueens51 {
+
+    int n;
+    List<List<String>> result = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) {
+        this.n = n;
+        char[][] grid = new char[n][n];
+        backTracking(grid, 0, 0);
+        return result;
+    }
+
+    public boolean backTracking(char[][] grid, int x, int y) {
+        if (y == n) {
+            addList(grid);
+            return true;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!check(grid, i, y)) {
+                continue;
+            }
+            grid[y][i] = 'Q';
+            if (backTracking(grid, i, y + 1)) {
+                grid[y][i] = 0;
+                continue;
+            }
+            grid[y][i] = 0;
+        }
+        return false;
+    }
+
+    public void addList(char[][] grid) {
+        List<String> tempList = new ArrayList<>();
+        for (int j = 0; j < n; j++) {
+            String str = "";
+            for (int i = 0; i < n; i++) {
+                if (grid[j][i] == 'Q') {
+                    str += 'Q';
+                } else {
+                    str += '.';
+                }
+            }
+            tempList.add(str);
+        }
+        result.add(tempList);
+    }
+
+    public boolean check(char[][] grid, int x, int y) {
+        for (int i = 0; i < n; i++) {
+            // line
+            if (grid[y][i] == 'Q') {
+                return false;
+            }
+            // column
+            if (grid[i][x] == 'Q') {
+                return false;
+            }
+            // left
+            if (y + i < n && x - i >= 0) {
+                if (grid[y + i][x - i] == 'Q') {
+                    return false;
+                }
+            }
+            if (y - i >= 0 && x + i < n) {
+                if (grid[y - i][x + i] == 'Q') {
+                    return false;
+                }
+            }
+            // right
+            if (y + i < n && x + i < n) {
+                if (grid[y + i][x + i] == 'Q') {
+                    return false;
+                }
+            }
+            if (y - i >= 0 && x - i >= 0) {
+                if (grid[y - i][x - i] == 'Q') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(new SolveNQueens51().solveNQueens(n));
+    }
 }
