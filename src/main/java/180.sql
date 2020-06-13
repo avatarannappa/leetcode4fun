4d3ed7bbd953518d6fd9828872f33921aa5a36e2
avatarannappa <nkcs.chenhao@gmail.com>
2020-06-13 13:22:32 +0800

    +add,180,连续出现的数字,sql

---
+++ src/main/java/180.sql
@@ -0,0 +1,13 @@
+# 180. 连续出现的数字 2020/6/13
+# Write your MySQL query statement below
+SELECT distinct l1.Num as ConsecutiveNums
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num
+;
