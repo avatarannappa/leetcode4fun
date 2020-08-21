cfae73fa8d9d660a36f973c3086b12915242ae6c
avatarannappa <nkcs.chenhao@gmail.com>
2020-08-21 11:24:14 +0800

    +add,30,串联所有单词的子串

---
+++ src/main/java/FindSubstring30.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 30. 串联所有单词的子串.
+ *
+ * @author avatarannappa
+ * @version 1.0, 2020/8/21
+ */
+public class FindSubstring30 {
+
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || s.length() == 0 || words == null || words.length == 0) {
+            return result;
+        }
+        Map<String, Integer> map = permuteUnique(words);
+        int wordLen = words[0].length();
+        int allLen = wordLen * words.length;
+        for (int i = 0; i <= s.length() - allLen; i++) {
+            String temp = s.substring(i, i + allLen);
+            Map<String, Integer> tempMap = new HashMap<>(map);
+            boolean sign = true;
+            for (int j = 0; j < temp.length(); j += wordLen) {
+                String word = temp.substring(j, j + wordLen);
+                if (!tempMap.containsKey(word)) {
+                    sign = false;
+                    break;
+                } else {
+                    int count = tempMap.get(word);
+                    if (count == 0) {
+                        sign = false;
+                        break;
+                    } else {
+                        tempMap.put(word, count - 1);
+                    }
+                }
+            }
+            if (sign) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+
+    public Map<String, Integer> permuteUnique(String[] words) {
+        Map<String, Integer> result = new HashMap<>();
+        for (String word : words) {
+            int count = result.getOrDefault(word, 0);
+            result.put(word, count + 1);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel";
+        String[] words = {"dhvf","sind","ffsl","yekr","zwzq","kpeo","cila","tfty","modg","ztjg","ybty","heqg","cpwo","gdcj","lnle","sefg","vimw","bxcb"};
+        System.out.println(new FindSubstring30().findSubstring(s, words));
+    }
+}
