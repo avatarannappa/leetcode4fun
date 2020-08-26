cf188fdade37d849e1e6b5ca4e016940c55974c7
avatarannappa <nkcs.chenhao@gmail.com>
2020-08-26 11:07:19 +0800

    +add,95,不同的二叉搜索树II,递归

--- src/main/java/GenerateTrees95.java
+++ src/main/java/GenerateTrees95.java
@@ -5,35 +5,48 @@ import java.util.List;
  * 95. 不同的二叉搜索树 II
  *
  * @author avatarannappa
- * @version 1.0, 2019/11/11
+ * @version 1.0, 2020/8/26
  */
 public class GenerateTrees95 {
+
+    List<TreeNode> result = new ArrayList<>();
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n < 1) {
+            return result;
+        }
+        return doRecursion(1, n);
+    }
+
+    public List<TreeNode> doRecursion(int left, int right) {
+        List<TreeNode> list = new ArrayList<>();
+
+        if (left > right) {
+            list.add(null);
+            return list;
+        }
+
+        for (int i = left; i <= right; i++) {
+            List<TreeNode> leftList = doRecursion(left, i - 1);
+            List<TreeNode> rightList = doRecursion(i + 1, right);
+            for (TreeNode l : leftList) {
+                for (TreeNode r : rightList) {
+                    TreeNode root = new TreeNode();
+                    root.val = i;
+                    root.left = l;
+                    root.right = r;
+                    list.add(root);
+                }
+            }
+        }
+
+        return list;
+    }
 
     public static void main(String[] args) {
         GenerateTrees95 g = new GenerateTrees95();
         int n = 3;
         List<TreeNode> result = g.generateTrees(n);
         System.out.println(result);
-    }
-
-    public List<TreeNode> generateTrees(int n) {
-        List<TreeNode> result = new ArrayList<>();
-        if (n == 0) {
-            return result;
-        } else if (n == 1) {
-            result.add(new TreeNode(1));
-            return result;
-        }
-        for (int i = 2; i <= n; i++) {
-            List<TreeNode> treeNode = doRecursion(i, n);
-            result.addAll(treeNode);
-        }
-        return result;
-    }
-
-    public List<TreeNode> doRecursion(int root, int n) {
-        List<TreeNode> treeNode = new ArrayList<>();
-
-        return treeNode;
     }
 }
