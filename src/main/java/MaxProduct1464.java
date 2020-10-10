a85d6c4b3ea78b8e643a19fc977d66a85790e865
avatarannappa <nkcs.chenhao@gmail.com>
2020-10-11 01:20:20 +0800

    +add,1464,easy,数组中两元素的最大乘积,O(n)

---
+++ src/main/java/MaxProduct1464.java
@@ -0,0 +1,24 @@
+/**
+ * 1464. 数组中两元素的最大乘积.
+ *
+ * @author avatarannappa
+ * @version 1.0, 2020/10/11
+ */
+public class MaxProduct1464 {
+
+    public int maxProduct(int[] nums) {
+        int a = Math.max(nums[0], nums[1]);
+        int b = Math.min(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            int num = nums[i];
+            if (num >= a) {
+                b = a;
+                a = num;
+            } else if (num > b) {
+                b = num;
+            }
+        }
+        return (a - 1) * (b - 1);
+    }
+
+}
