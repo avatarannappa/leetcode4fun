6f7ef21f70a90702af3ce14febf4bd878eefd890
avatarannappa <nkcs.chenhao@gmail.com>
2020-10-14 10:38:30 +0800

    +add,701,二叉搜索树中的插入操作

---
+++ src/main/java/InsertIntoBST701.java
@@ -0,0 +1,32 @@
+/**
+ * 701. 二叉搜索树中的插入操作.
+ *
+ * @author avatarannappa
+ * @version 1.0, 2020/10/14
+ */
+public class InsertIntoBST701 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        TreeNode pos = root;
+        while (pos != null) {
+            if (val < pos.val) {
+                if (pos.left == null) {
+                    pos.left = new TreeNode(val);
+                    break;
+                } else {
+                    pos = pos.left;
+                }
+            } else {
+                if (pos.right == null) {
+                    pos.right = new TreeNode(val);
+                    break;
+                } else {
+                    pos = pos.right;
+                }
+            }
+        }
+        return root;
+    }
+}
