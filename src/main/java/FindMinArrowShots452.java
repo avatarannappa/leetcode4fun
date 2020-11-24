7f6774f90695df02183f81327bdb670fe752b6b4
avatarannappa <nkcs.chenhao@gmail.com>
2020-11-24 21:10:22 +0800

    #update,452,medium,用最少数量的箭引爆气球,array sort

--- src/main/java/FindMinArrowShots452.java
+++ src/main/java/FindMinArrowShots452.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +15,29 @@ import java.util.TreeMap;
  */
 public class FindMinArrowShots452 {
 
+    public int findMinArrowShotsNew(int[][] points) {
+        // array排序
+        if (points == null || points.length < 1) {
+            return 0;
+        }
+
+        // 同 Arrays.sort(points, (o1, o2) -> Integer.compare(o1[0], o2[0]));
+        Arrays.sort(points, Comparator.comparingInt(o -> o[1]));
+
+        int arrow = 1;
+        int end = points[0][1];
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > end) {
+                arrow++;
+                end = points[i][1];
+            }
+        }
+
+        return arrow;
+    }
+
     public int findMinArrowShots(int[][] points) {
+        // 排序+贪心
         if (points == null || points.length < 1) {
             return 0;
         }
@@ -51,7 +75,7 @@ public class FindMinArrowShots452 {
     public static void main(String[] args) {
         // int[][] points = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
         int[][] points = {{1, 2}, {4, 5}, {1, 5}};
-        System.out.println(new FindMinArrowShots452().findMinArrowShots(points));
+        System.out.println(new FindMinArrowShots452().findMinArrowShotsNew(points));
 
         int a = 0;
         int b = 0;
