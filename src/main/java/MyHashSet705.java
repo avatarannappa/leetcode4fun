35f55e93291dc6bf810517286aefbe44222759d6
avatarannappa <nkcs.chenhao@gmail.com>
2021-03-13 23:50:14 +0800

    +add,705,easy,设计哈希集合

---
+++ src/main/java/MyHashSet705.java
@@ -0,0 +1,63 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * 705. 设计哈希集合
+ * easy
+ *
+ * @author avatarannappa
+ * @version 1.0, 2021/3/13
+ */
+public class MyHashSet705 {
+    private static final int BASE = 769;
+    private LinkedList[] data;
+
+    /** Initialize your data structure here. */
+    public MyHashSet705() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE; ++i) {
+            data[i] = new LinkedList<Integer>();
+        }
+    }
+
+    public void add(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return;
+            }
+        }
+        data[h].offerLast(key);
+    }
+
+    public void remove(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                data[h].remove(element);
+                return;
+            }
+        }
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int hash(int key) {
+        return key % BASE;
+    }
+}
