263a5c9d4e89861e461e6f1d04dc38a52fb3e261
avatarannappa <nkcs.chenhao@gmail.com>
2021-07-05 14:44:19 +0800

    +add,516,medium,最长回文子序列,DP

---
+++ src/main/java/LongestPalindromeSubseq516.java
@@ -0,0 +1,32 @@
+/**
+ * 516. 最长回文子序列
+ * medium
+ *
+ * @author avatarannappa
+ * @version 1.0, 2021/7/5
+ */
+public class LongestPalindromeSubseq516 {
+
+    public int longestPalindromeSubseq(String s) {
+        int len = s.length();
+        int[][] dp = new int[len][len];
+        for (int i = len - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < len; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][len - 1];
+    }
+
+    public static void main(String[] args) {
+        String s = "bcbcbcacb";
+        // 7
+        System.out.println(new LongestPalindromeSubseq516().longestPalindromeSubseq(s));
+    }
+
+}
