0022666ad1bfc0b5119c24e44814fc71a811e399
avatarannappa <nkcs.chenhao@gmail.com>
2021-09-23 10:18:34 +0800

    +add,322,medium,DP 类似204,640,埃氏筛

--- src/main/java/CoinChange322.java
+++ src/main/java/CoinChange322.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
  *
  * @author avatarannappa
  * @version 1.0, 2020/3/10
+ * @since 2021.09.23 DP O(n*coins.length)
  */
 public class CoinChange322 {
 
@@ -12,14 +13,30 @@ public class CoinChange322 {
     int[] coins = null;
 
     public int coinChange(int[] coins, int amount) {
+        // DP 类似于埃氏筛，204题 质数，650题 两个键的键盘
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]]);
+                }
+            }
+            dp[i] += 1;
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public int coinChange1(int[] coins, int amount) {
         // 超出时间限制
         Arrays.sort(coins);
         this.coins = coins;
-        recursion(coins.length - 1, amount, 0);
+        recursion1(coins.length - 1, amount, 0);
         return count == Integer.MAX_VALUE ? -1 : count;
     }
 
-    public void recursion(int index, int amount, int step) {
+    public void recursion1(int index, int amount, int step) {
         if (index < 0) {
             return;
         }
@@ -33,7 +50,7 @@ public class CoinChange322 {
             if (step + i > count) {
                 break;
             }
-            recursion(index - 1, amount - i * coin, step + i);
+            recursion1(index - 1, amount - i * coin, step + i);
         }
     }
 
@@ -72,8 +89,10 @@ public class CoinChange322 {
     }
 
     public static void main(String[] args) {
-        int[] coins = {288, 160, 10, 249, 40, 77, 314, 429};
-        int amount = 9208;
+        // int[] coins = { 288, 160, 10, 249, 40, 77, 314, 429 };
+        // int amount = 9208;
+        int[] coins = { 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422 };
+        int amount = 9864;
         System.out.println(new CoinChange322().coinChange(coins, amount));
     }
 }
