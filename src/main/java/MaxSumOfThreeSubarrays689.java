65fff1cf250d743c30c19512ef3a3469db2de396
avatarannappa <nkcs.chenhao@gmail.com>
2022-01-05 11:00:02 +0800

    +add,689,hard,三个无重叠子数组的最大和,DP

---
+++ src/main/java/MaxSumOfThreeSubarrays689.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+/**
+ * 689. 三个无重叠子数组的最大和.
+ * hard
+ *
+ * @author avatarannappa
+ * @version 1.0, 2022/1/5
+ */
+public class MaxSumOfThreeSubarrays689 {
+
+    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        long[] sum = new long[n + 1];
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+        long[][] f = new long[n + 10][4];
+        for (int i = n - k + 1; i >= 1; i--) {
+            for (int j = 1; j < 4; j++) {
+                f[i][j] = Math.max(f[i + 1][j], f[i + k][j - 1] + sum[i + k - 1] - sum[i - 1]);
+            }
+        }
+        int[] ans = new int[3];
+        int i = 1, j = 3, idx = 0;
+        while (j > 0) {
+            if (f[i + 1][j] > f[i + k][j - 1] + sum[i + k - 1] - sum[i - 1]) {
+                i++;
+            } else {
+                ans[idx++] = i - 1;
+                i += k;
+                j--;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1, 2, 6, 7, 5, 1};
+        int k = 2;
+        // [0,3,5]
+        System.out.println(Arrays.toString(new MaxSumOfThreeSubarrays689().maxSumOfThreeSubarrays(nums, k)));
+    }
+}
