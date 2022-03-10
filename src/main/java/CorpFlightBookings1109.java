19fe1c384307a5fcd44e0e74e25c40476300fa32
avatarannappa <nkcs.chenhao@gmail.com>
2022-03-10 17:10:29 +0800

    +add,1109,medium,航班预订统计,差分+前缀和

--- src/main/java/CorpFlightBookings1109.java
+++ src/main/java/CorpFlightBookings1109.java
@@ -9,7 +9,26 @@
 public class CorpFlightBookings1109 {
 
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        // 模拟
+        // 差分 O(m + n)
+        int[] cf = new int[n + 1];
+        for (int[] b : bookings) {
+            int l = b[0] - 1;
+            int r = b[1];
+            int v = b[2];
+            cf[l] += v;
+            cf[r] -= v;
+        }
+        
+        int[] ans = new int[n];
+        ans[0] = cf[0];
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] + cf[i];
+        }
+        return ans;
+    }
+    
+    public int[] corpFlightBookingsOld(int[][] bookings, int n) {
+        // 模拟 O(m * n)
         int[] ans = new int[n];
 
         for (int i = 0; i < bookings.length; i++) {
