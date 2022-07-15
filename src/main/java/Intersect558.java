bff48e6362177f5f1bff123ff6d945c979d63b21
avatarannappa <nkcs.chenhao@gmail.com>
2022-07-15 15:02:49 +0800

    +add,558,medium,四叉树交集,递归recursion

---
+++ src/main/java/Intersect558.java
@@ -0,0 +1,69 @@
+/**
+ * 558. 四叉树交集
+ * medium
+ *
+ * @author chenhao138
+ * @date 2022/7/15
+ */
+public class Intersect558 {
+
+    public Node intersect(Node quadTree1, Node quadTree2) {
+        // 递归
+        if (quadTree1 == null && quadTree2 == null) {
+            return null;
+        } else if (quadTree1 == null) {
+            return quadTree2;
+        } else if (quadTree2 == null) {
+            return quadTree1;
+        }
+        Node res;
+        if ((quadTree1.isLeaf && quadTree1.val) || (quadTree2.isLeaf && quadTree2.val)) {
+            res = new Node(true, true, null, null, null, null);
+            return res;
+        } else if (quadTree1.isLeaf && quadTree2.isLeaf) {
+            res = new Node(false, true, null, null, null, null);
+            return res;
+        }
+
+        res = new Node();
+        res.topLeft = intersect(quadTree1.topLeft, quadTree2.topLeft);
+        res.topRight = intersect(quadTree1.topRight, quadTree2.topRight);
+        res.bottomLeft = intersect(quadTree1.bottomLeft, quadTree2.bottomLeft);
+        res.bottomRight = intersect(quadTree1.bottomRight, quadTree2.bottomRight);
+        // 合并
+        if (res.topLeft.isLeaf && res.topRight.isLeaf && res.bottomLeft.isLeaf && res.bottomRight.isLeaf) {
+            if (res.topLeft.val && res.topRight.val && res.bottomLeft.val && res.bottomRight.val) {
+                res = new Node(true, true, null, null, null, null);
+                return res;
+            } else if (!res.topLeft.val && !res.topRight.val && !res.bottomLeft.val && !res.bottomRight.val) {
+                res = new Node(false, true, null, null, null, null);
+                return res;
+            }
+        }
+        return res;
+    }
+
+    class Node {
+
+        public boolean val;
+        public boolean isLeaf;
+        public Node topLeft;
+        public Node topRight;
+        public Node bottomLeft;
+        public Node bottomRight;
+
+        public Node() {
+        }
+
+        public Node(boolean _val, boolean _isLeaf, Node _topLeft, Node _topRight, Node _bottomLeft, Node _bottomRight) {
+            val = _val;
+            isLeaf = _isLeaf;
+            topLeft = _topLeft;
+            topRight = _topRight;
+            bottomLeft = _bottomLeft;
+            bottomRight = _bottomRight;
+        }
+    }
+
+    ;
+}
