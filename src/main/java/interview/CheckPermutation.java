0a15f28e7f7b06b295f3b20e98307190b00736ce
avatarannappa <nkcs.chenhao@gmail.com>
2022-09-27 10:21:07 +0800

    #update,fix 面试题 01.02

--- src/main/java/interview/CheckPermutation.java
+++ src/main/java/interview/CheckPermutation.java
@@ -27,17 +27,5 @@ public class CheckPermutation {
             }
         }
         return true;
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
-        for (int i = 0; i < s1.length(); i++) {
-            map1.put(s1.charAt(i), map1.getOrDefault(s1.charAt(i), 0) + 1);
-            map2.put(s2.charAt(i), map2.getOrDefault(s2.charAt(i), 0) + 1);
-        }
-        for (Character key : map1.keySet()) {
-            if (map1.get(key) != map2.get(key)) {
-                return false;
-            }
-        }
-        return true;
     }
 }
