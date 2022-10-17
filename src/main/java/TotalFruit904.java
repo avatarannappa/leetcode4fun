8e232c03f44a5c188befc7149202442f524c648f
avatarannappa <nkcs.chenhao@gmail.com>
2022-10-17 16:17:59 +0800

    #update,904,记录变化点

--- src/main/java/TotalFruit904.java
+++ src/main/java/TotalFruit904.java
@@ -31,13 +31,42 @@ public class TotalFruit904 {
         return ans;
     }
 
+    public int totalFruitNew(int[] fruits) {
+        // 滑动窗口，去掉内部while循环.
+        // 表现并不好，可能是计算太多了
+        int ans = 0;
+
+        int[] cnts = new int[fruits.length + 10];
+        int bucket = 0;
+        int changeIndex = 0;
+        for (int i = 0, j = 0; i < fruits.length; i++) {
+            cnts[fruits[i]]++;
+            if (cnts[fruits[i]] == 1) {
+                bucket++;
+            }
+            if (i > 0 && bucket <= 2 && fruits[i] != fruits[i - 1]) {
+                changeIndex = i;
+            }
+            if (bucket > 2) {
+                bucket--;
+                cnts[fruits[i - 1]] -= changeIndex - j - cnts[fruits[changeIndex - 1]];
+                cnts[fruits[changeIndex - 1]] = 0;
+
+                j = changeIndex;
+                changeIndex = i;
+            }
+            ans = Math.max(ans, i - j + 1);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         // 输入：fruits = [3,3,3,1,2,1,1,2,3,3,4]
         // 输出：5
         // 解释：可以采摘 [1,2,1,1,2] 这五棵树。
         // int[] fruits = {3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4};
-        int[] fruits = {6, 2, 1, 1, 3, 6, 6};
-        // 3
-        System.out.println(new TotalFruit904().totalFruitOld(fruits));
+        int[] fruits = {1, 0, 1, 4, 1, 4, 1, 2, 3};
+        // 5
+        System.out.println(new TotalFruit904().totalFruitNew(fruits));
     }
 }
