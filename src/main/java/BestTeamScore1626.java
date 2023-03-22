2830014ac2d7cbaab9cb0af86cdb980a6f13ca25
avatarannappa <nkcs.chenhao@gmail.com>
2023-03-22 12:05:09 +0800

    #update,1626,medium,无矛盾的最佳球队,另一种排序+dp(最长递增子序列),star

--- src/main/java/BestTeamScore1626.java
+++ src/main/java/BestTeamScore1626.java
@@ -32,10 +32,33 @@ public class BestTeamScore1626 {
         return res;
     }
 
+    public int bestTeamScoreNew(int[] scores, int[] ages) {
+        // dp 最长递增子序列
+        int n = scores.length;
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; ++i) {
+            arr[i] = new int[]{ages[i], scores[i]};
+        }
+        Arrays.sort(arr, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+
+        int[] dp = new int[n];
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j][1] <= arr[i][1]) {
+                    dp[i] = Math.max(dp[i], dp[j]);
+                }
+            }
+            dp[i] += arr[i][1];
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] scores = {1, 3, 7, 3, 2, 4, 10, 7, 5};
         int[] ages = {4, 5, 2, 1, 1, 2, 4, 1, 4};
         // 29
-        System.out.println(new BestTeamScore1626().bestTeamScore(scores, ages));
+        System.out.println(new BestTeamScore1626().bestTeamScoreNew(scores, ages));
     }
 }
