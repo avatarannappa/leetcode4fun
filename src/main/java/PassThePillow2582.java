21a994587091b3ae0bf850a34d0fe2ba55e499e2
avatarannappa <nkcs.chenhao@gmail.com>
2023-09-26 09:52:04 +0800

    +add,2582,easy,递枕头,规律数学

---
+++ src/main/java/PassThePillow2582.java
@@ -0,0 +1,19 @@
+/**
+ * 2582. 递枕头
+ * easy
+ *
+ * @author avatarannappa
+ * @version 1.0, 2023/09/26
+ */
+public class PassThePillow2582 {
+
+    public int passThePillow(int n, int time) {
+
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int time = 5;
+        System.out.println(new PassThePillow2582().passThePillow(n, time));
+    }
+}
