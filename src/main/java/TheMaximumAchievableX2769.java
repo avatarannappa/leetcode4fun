644beafb7ed7363ee94e2503112b5e1678c9c169
avatarannappa <nkcs.chenhao@gmail.com>
2024-05-21 10:39:46 +0800

    +add,2769,easy,找出最大的可达成数字,简单模拟

---
+++ src/main/java/TheMaximumAchievableX2769.java
@@ -0,0 +1,14 @@
+/**
+ * 2769. 找出最大的可达成数字
+ * easy
+ *
+ * @author avatarannappa
+ * @date 2024/5/21
+ */
+public class TheMaximumAchievableX2769 {
+
+    public int theMaximumAchievableX(int num, int t) {
+        // 简单模拟
+        return num + 2 * t;
+    }
+}
