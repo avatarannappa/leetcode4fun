5a68a296dfe469ccf82734d17b756abeb1773558
avatarannappa <nkcs.chenhao@gmail.com>
2025-03-05 15:59:44 +0800

    #update,1745,hard,分割回文串 IV,增加记忆化递推方法,star

--- src/main/java/CheckPartitioning1745.java
+++ src/main/java/CheckPartitioning1745.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 1745. 分割回文串 IV.
  * hard
@@ -7,13 +9,14 @@
  */
 public class CheckPartitioning1745 {
 
+    char[] s;
+
     public boolean checkPartitioning(String s) {
-        return palindromePartition(s, 3) == 0;
+        return palindromePartitionNew(s, 3) == 0;
     }
 
-    // 1278. 分割回文串 III
-    private int palindromePartition(String S, int k) {
-        // 递归
+    private int palindromePartitionNew(String S, int k) {
+        // 递推
         char[] s = S.toCharArray();
         int n = s.length;
         int[][] minChange = new int[n][n];
@@ -33,6 +36,59 @@ public class CheckPartitioning1745 {
             }
         }
         return f[n - 1];
+    }
+
+    // 1278. 分割回文串 III
+    private int palindromePartition(String S, int k) {
+        // 递归
+        int n = S.length();
+        this.s = S.toCharArray();
+
+        int[][] mem = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            // 缓存change结果
+            Arrays.fill(mem[i], -1);
+        }
+        int[][] dMem = new int[n][k];
+        for (int i = 0; i < n; i++) {
+            // 缓存dfs结果
+            Arrays.fill(dMem[i], -1);
+        }
+        return dfs(n - 1, k - 1, mem, dMem);
+    }
+
+    public int dfs(int r, int k, int[][] mem, int[][] dMemo) {
+        // i是s右index，k是要分成k个串
+        if (k == 0) {
+            return change(0, r, mem);
+        }
+        if (dMemo[r][k] != -1) {
+            return dMemo[r][k];
+        }
+        int res = Integer.MAX_VALUE;
+        for (int rr = k; rr <= r; rr++) {
+            res = Math.min(res, dfs(rr - 1, k - 1, mem, dMemo) + change(rr, r, mem));
+        }
+        return dMemo[r][k] = res;
+    }
+
+    public int change(int l, int r, int[][] mem) {
+        if (l >= r) {
+            return 0;
+        }
+        if (mem[l][r] != -1) {
+            return mem[l][r];
+        }
+        int res = change(l + 1, r - 1, mem);
+        if (s[l] != s[r]) {
+            res++;
+        }
+        return mem[l][r] = res;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcbdd";
+        System.out.println(new CheckPartitioning1745().checkPartitioning(s));
     }
 
 }
