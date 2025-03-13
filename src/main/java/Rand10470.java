1780d6e0c5daf91df8f2919f06fee9b97f976498
avatarannappa <nkcs.chenhao@gmail.com>
2025-03-13 10:11:12 +0800

    #update,470,medium,用 Rand7() 实现 Rand10(),扩展实现,star

--- src/main/java/Rand10470.java
+++ src/main/java/Rand10470.java
@@ -8,13 +8,33 @@ import java.util.Random;
  */
 public class Rand10470 {
 
+    public int rand10Old() {
+        // 独立事件，比较慢
+        while (true) {
+            int r = 7 * (rand7() - 1) + rand7() - 1;
+            if (r >= 1 && r <= 10) {
+                return r;
+            }
+        }
+    }
+
     public int rand10() {
-        // 独立事件 -> 打表 -> 进制
+        // 加速。独立事件 -> 打表 -> 进制
         int num = Integer.MAX_VALUE;
         while (num > 40) {
             num = (rand7() - 1) * 7 + rand7() - 1;
         }
         return num % 10 + 1;
+    }
+    
+    public int rand2() {
+        // 加速
+        while (true) {
+            int r = rand7();
+            if (r <= 6) {
+                return r % 2 + 1;
+            }
+        }
     }
 
     public int rand7() {
