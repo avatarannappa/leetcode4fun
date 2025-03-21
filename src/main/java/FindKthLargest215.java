601db718d38ce82edb45a4d772afabefd8cb455e
avatarannappa <nkcs.chenhao@gmail.com>
2025-03-21 18:34:42 +0800

    +add,2680,medium,最大或值,贪心+前后缀和,star

--- src/main/java/FindKthLargest215.java
+++ src/main/java/FindKthLargest215.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 /**
  * 215. 数组中的第K个最大元素.
@@ -8,7 +9,23 @@ import java.util.Arrays;
  */
 public class FindKthLargest215 {
 
+    public int findKthLargestHeap(int[] nums, int k) {
+        // 堆
+        // TODO 自己实现堆
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            queue.add(nums[i]);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+        return queue.peek();
+    }
+
     public int findKthLargest(int[] nums, int k) {
+        // 排序
+        // TODO 自己实现快排
         Arrays.sort(nums);
         return nums[nums.length - k];
     }
