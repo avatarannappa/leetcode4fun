ce6f1ca9ac478229b7641cba1a565685f732009b
avatarannappa <nkcs.chenhao@gmail.com>
2025-03-25 18:30:48 +0800

    #update,200,medium,岛屿数量,BFS&DFS,star

--- src/main/java/NumIslands200.java
+++ src/main/java/NumIslands200.java
@@ -1,5 +1,9 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -9,6 +13,66 @@ import java.util.Queue;
  * @version 1.0, 2020/4/21
  */
 public class NumIslands200 {
+
+    public int numIslandsDFS(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+    }
+
+    public int numIslandsBFS(char[][] grid) {
+        int count = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    bfs(grid, i, j, visited);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    int[][] mask = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    private void bfs(char[][] grid, int i, int j, boolean[][] visited) {
+        Deque<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{i, j});
+        while (!q.isEmpty()) {
+            List<int[]> l = new ArrayList<>();
+            while (!q.isEmpty()) {
+                int[] p = q.poll();
+                for (int[] arrow : mask) {
+                    int x = p[0] + arrow[0];
+                    int y = p[1] + arrow[1];
+                    if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && !visited[x][y] && grid[x][y] == '1') {
+                        visited[x][y] = true;
+                        l.add(new int[]{x, y});
+                    }
+                }
+            }
+            q.addAll(l);
+        }
+    }
 
     public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0) {
@@ -33,7 +97,7 @@ public class NumIslands200 {
                         while (!queue.isEmpty()) {
                             int[] item = queue.poll();
                             if (item[1] + 1 < width && grid[item[0]][item[1] + 1] == '1' &&
-                                visited[item[0]][item[1] + 1] == 0) {
+                                    visited[item[0]][item[1] + 1] == 0) {
                                 // 右
                                 int[] newItem = new int[2];
                                 newItem[0] = item[0];
@@ -42,7 +106,7 @@ public class NumIslands200 {
                                 visited[item[0]][item[1] + 1] = 1;
                             }
                             if (item[1] - 1 >= 0 && grid[item[0]][item[1] - 1] == '1' &&
-                                visited[item[0]][item[1] - 1] == 0) {
+                                    visited[item[0]][item[1] - 1] == 0) {
                                 // 左
                                 int[] newItem = new int[2];
                                 newItem[0] = item[0];
@@ -51,7 +115,7 @@ public class NumIslands200 {
                                 visited[item[0]][item[1] - 1] = 1;
                             }
                             if (item[0] + 1 < height && grid[item[0] + 1][item[1]] == '1' &&
-                                visited[item[0] + 1][item[1]] == 0) {
+                                    visited[item[0] + 1][item[1]] == 0) {
                                 // 下
                                 int[] newItem = new int[2];
                                 newItem[0] = item[0] + 1;
@@ -60,7 +124,7 @@ public class NumIslands200 {
                                 visited[item[0] + 1][item[1]] = 1;
                             }
                             if (item[0] - 1 >= 0 && grid[item[0] - 1][item[1]] == '1' &&
-                                visited[item[0] - 1][item[1]] == 0) {
+                                    visited[item[0] - 1][item[1]] == 0) {
                                 // 上
                                 int[] newItem = new int[2];
                                 newItem[0] = item[0] - 1;
