1e03565e0ab5382009487517844e642b29a9d409
avatarannappa <nkcs.chenhao@gmail.com>
2025-04-20 11:42:03 +0800

    #update,781,medium,

--- src/main/java/NumRabbits781.java
+++ src/main/java/NumRabbits781.java
@@ -8,10 +8,38 @@ import java.util.Map;
  *
  * @author avatarannappa
  * @version 1.0, 2021/4/4
+ * @version 1.1, 2025.4.20
  */
 public class NumRabbits781 {
 
     public int numRabbits(int[] answers) {
+        // 规律题
+        int result = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int a : answers) {
+            if (a == 0) {
+                result++;
+            } else {
+                map.put(a, map.getOrDefault(a, 0) + 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int key = entry.getKey();
+            int val = entry.getValue();
+            int cnt = key + 1;
+            while (val > 0) {
+                // 说该数的兔子比该颜色兔子数多，则不能是同一种颜色，需要另起一种颜色
+                result = result + cnt;
+                val -= cnt;
+            }
+        }
+
+        return result;
+    }
+
+    public int numRabbitsOld(int[] answers) {
         if (answers == null || answers.length == 0) {
             return 0;
         }
